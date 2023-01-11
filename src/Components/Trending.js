7f76b8c1422a79fb9c6374ae43dafdc34ad1c91d
Anugrah Ardhi Latief Maulana <97450055+Lateeep@users.noreply.github.com>
2023-01-11 22:39:20 +0700

    Update Trending.js

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -1,101 +1,108 @@
-import { Card, Col, Container, Row, Image } from "react-bootstrap"
-import avatarImage from "../assets/Trending/Avatar.jpg"
-import fast9Image from "../assets/Trending/Fast9.jpg"
-import m3ganImage from "../assets/Trending/M3gan.jpg"
-import venomImage from "../assets/Trending/Venom.jpg"
-import theraidImage from "../assets/Trending/theraid.jpg"
-import avengerImage from "../assets/Trending/Avenger.jpg"
+import { Card, Col, Container, Row, Image, Button } from "react-bootstrap"
+import diehardImage from "../assets/Action/Diehard.jpg"
+import headshotImage from "../assets/Action/Headshot.jpg"
+import madmaxImage from "../assets/Action/Mad Max.jpg"
+import matrixImage from "../assets/Action/Matrix.jpg"
+import mile22Image from "../assets/Action/Mile 22.jpg"
+import monsterhunterImage from "../assets/Action/MonsterHunter.jpg"
 
-const Trending = () => {
+
+const Action = () => {
     return (
         <div>
             <Container>
                 <br />
-                <h1 className="text-white text-center" id="trending">TRENDING MOVIES</h1>
+                <h1 className="text-white text-center" id="action">ACTION MOVIES</h1>
                 <br />
                 <Row>
+                    <Col md={4} className="movieWrapper" >
+                        <Card className="bg-dark text-light movieImages">
+                            <Image
+                                src={diehardImage}
+                                alt="DieHard Movie"
+                                className="Poster"
+                            />
+                            <Card.Title className="text-center">DIE HARD</Card.Title>
+                            <Card.Text className="text-center">
+                                Action, Thriller
+                            </Card.Text>
+                            <Card.Text className="text-center">2h 12m</Card.Text>
+                            <Button a href="https://www.youtube.com/watch?v=jaJuwKCmJbY">Watch Trailer</Button>
+                        </Card>
+                    </Col>
                     <Col md={4} className="movieWrapper">
                         <Card className="bg-dark text-light movieImages">
                             <Image
-                                src={avatarImage}
-                                alt="Avatar Movie"
+                                src={headshotImage}
+                                alt="Headshot Movie"
                                 className="Poster"
                             />
-                            <Card.Title className="text-center">AVATAR: THE WAY OF WATER</Card.Title>
+                            <Card.Title className="text-center">HEADSHOT</Card.Title>
+                            <Card.Text className="text-center">
+                                Action, Drama, Thriller
+                            </Card.Text>
+                            <Card.Text className="text-center">1h 58m</Card.Text>
+                            <Button a href="https://www.youtube.com/watch?v=yXhoytk0Hfw">Watch Trailer</Button>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="bg-dark text-light movieImages">
+                            <Image
+                                src={madmaxImage}
+                                alt="Madmax Movie"
+                                className="Poster"
+                            />
+                            <Card.Title className="text-center">MAD MAX: FURY ROAD</Card.Title>
+                            <Card.Text className="text-center">
+                                Action, Adventure, Sci-Fi
+                            </Card.Text>
+                            <Card.Text className="text-center">2h</Card.Text>
+                            <Button a href="https://www.youtube.com/watch?v=hEJnMQG9ev8">Watch Trailer</Button>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="bg-dark text-light movieImages">
+                            <Image
+                                src={matrixImage}
+                                alt="Matrix Movie"
+                                className="Poster"
+                            />
+                            <Card.Title className="text-center">THE MATRIX</Card.Title>
+                            <Card.Text className="text-center">
+                                Action, Sci-Fi
+                            </Card.Text>
+                            <Card.Text className="text-center">2h 16m</Card.Text>
+                            <Button a href="https://www.youtube.com/watch?v=vKQi3bBA1y8">Watch Trailer</Button>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="bg-dark text-light movieImages">
+                            <Image
+                                src={mile22Image}
+                                alt="Mile22 Movie"
+                                className="Poster"
+                            />
+                            <Card.Title className="text-center">MILE 22</Card.Title>
+                            <Card.Text className="text-center">
+                                Action, Thriller
+                            </Card.Text>
+                            <Card.Text className="text-center">1h 34m</Card.Text>
+                            <Button a href="https://www.youtube.com/watch?v=eJU6S5KOsNI">Watch Trailer</Button>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrapper">
+                        <Card className="bg-dark text-light movieImages">
+                            <Image
+                                src={monsterhunterImage}
+                                alt="Monsterhunter Movie"
+                                className="Poster"
+                            />
+                            <Card.Title className="text-center">MONSTER HUNTER</Card.Title>
                             <Card.Text className="text-center">
                                 Action, Adventure, Fantasy
                             </Card.Text>
-                            <Card.Text className="text-center">3h 12m</Card.Text>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="bg-dark text-light movieImages">
-                            <Image
-                                src={fast9Image}
-                                alt="F9 Movie"
-                                className="Poster"
-                            />
-                            <Card.Title className="text-center">F9: THE FAST SAGA</Card.Title>
-                            <Card.Text className="text-center">
-                                Action, Thriller, Crime
-                            </Card.Text>
-                            <Card.Text className="text-center">2h 23m</Card.Text>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="bg-dark text-light movieImages">
-                            <Image
-                                src={m3ganImage}
-                                alt="M3gan Movie"
-                                className="Poster"
-                            />
-                            <Card.Title className="text-center">M3GAN</Card.Title>
-                            <Card.Text className="text-center">
-                                Horror, Sci-Fi, Thriller
-                            </Card.Text>
-                            <Card.Text className="text-center">1h 42m</Card.Text>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="bg-dark text-light movieImages">
-                            <Image
-                                src={venomImage}
-                                alt="Venom Movie"
-                                className="Poster"
-                            />
-                            <Card.Title className="text-center">VENOM: LET THERE BE CARNAGE</Card.Title>
-                            <Card.Text className="text-center">
-                                Action, Adventure, Sci-Fi
-                            </Card.Text>
-                            <Card.Text className="text-center">1h 37m</Card.Text>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="bg-dark text-light movieImages">
-                            <Image
-                                src={theraidImage}
-                                alt="Theraid Movie"
-                                className="Poster"
-                            />
-                            <Card.Title className="text-center">THE RAID: REDEMPTION</Card.Title>
-                            <Card.Text className="text-center">
-                                Action, Crime, Thriller
-                            </Card.Text>
-                            <Card.Text className="text-center">1h 41m</Card.Text>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="bg-dark text-light movieImages">
-                            <Image
-                                src={avengerImage}
-                                alt="Avenger Movie"
-                                className="Poster"
-                            />
-                            <Card.Title className="text-center">AVENGERS: INFINITY WAR</Card.Title>
-                            <Card.Text className="text-center">
-                                Action, Adventure, Sci-Fi
-                            </Card.Text>
-                            <Card.Text className="text-center">2h 29m</Card.Text>
+                            <Card.Text className="text-center">1h 43m</Card.Text>
+                            <Button a href="https://www.youtube.com/watch?v=3od-kQMTZ9M">Watch Trailer</Button>
                         </Card>
                     </Col>
                 </Row>
@@ -104,4 +111,4 @@ const Trending = () => {
     )
 }
 
-export default Trending
+export default Action
